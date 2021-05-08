c32b8beee8e2de38bd7633745ff69bc9c5929365
rstrzecha <rstrzecha@wp.pl>
2021-05-08 14:48:28 +0200

    ActiveEmployee entity added

---
+++ src/main/java/com/example/interview_task/entity/ActiveEmployee.java
@@ -0,0 +1,40 @@
+package com.example.interview_task.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class ActiveEmployee {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private Double salary;
+    private LocalDate dateOfEmployment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getDateOfEmployment() {
+        return dateOfEmployment;
+    }
+
+    public void setDateOfEmployment(LocalDate dateOfEmployment) {
+        this.dateOfEmployment = dateOfEmployment;
+    }
+}
--- src/main/java/com/example/interview_task/entity/Employee.java
+++ src/main/java/com/example/interview_task/entity/Employee.java
@@ -13,5 +13,19 @@ public class Employee {
     @Column(length = 50)
     private String name;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
