bcd572e37009683204f1ff06ccc25d5a824af460
rstrzecha <rstrzecha@wp.pl>
2021-05-11 12:08:36 +0200

    Edditing Active Employees fields feature

--- src/main/java/com/example/interview_task/controller/ActiveEmployeeController.java
+++ src/main/java/com/example/interview_task/controller/ActiveEmployeeController.java
@@ -4,12 +4,13 @@ import com.example.interview_task.dao.ActiveEmployeeRepo;
 import com.example.interview_task.entity.ActiveEmployee;
 import com.example.interview_task.service.ActiveEmployeeManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ActiveEmployeeController {
@@ -31,6 +32,37 @@ public class ActiveEmployeeController {
     @PostMapping("/addActive")
     public ActiveEmployee addActiveEmployee(@RequestBody ActiveEmployee newEmployee) {
         return activeEmployeeManager.save(newEmployee);
+    }
+
+    @PutMapping("/{id}/newSalary/{salary}")
+    public ResponseEntity<ActiveEmployee> modEmployeeSalary(@PathVariable ("id") Long id,
+                                                            @PathVariable("salary") String newSalary) {
+
+        Optional<ActiveEmployee> foundEmployee = activeEmployeeManager.findById(id);
+        if(foundEmployee.isPresent()) {
+            ActiveEmployee newActiveEmployee = foundEmployee.get();
+            newActiveEmployee.setSalary(Double.parseDouble(newSalary));
+            newActiveEmployee.setId(id);
+            activeEmployeeManager.save(newActiveEmployee);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}/newDateOfEmployment/{dateOfEmployment}")
+    public ResponseEntity<ActiveEmployee> modEmployeeDate(@PathVariable ("id") Long id,
+                                                            @PathVariable("dateOfEmployment") String newDateOfEmployment) {
+
+        Optional<ActiveEmployee> foundEmployee = activeEmployeeManager.findById(id);
+        if(foundEmployee.isPresent()) {
+            ActiveEmployee newActiveEmployee = foundEmployee.get();
+            newActiveEmployee.setDateOfEmployment(LocalDate.parse(newDateOfEmployment));          /*setSalary(Double
+            .parseDouble(newSalary));*/
+            newActiveEmployee.setId(id);
+            activeEmployeeManager.save(newActiveEmployee);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
